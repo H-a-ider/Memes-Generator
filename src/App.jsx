979d0dfc7f84770c64cc1e72777a005bd3542520
Zeeshan Haider <zeeshanhaider1902@gmail.com>
2023-07-24 15:36:22 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Header from "./Header"
+import Meme from "./Meme"
+import "./style.css"
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
